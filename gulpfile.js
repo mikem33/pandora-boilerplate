bb7448f179cdf5e9e54bf2a5192dddb49c685968
mikem33 <mikem33@gmail.com>
2018-06-07 13:24:06 +0200

    A lot of changes. I have removed Bower from the project and now all it is depending on NPM. With this decission I had to change the way the CSS and JS files are linking in the main document and now the src folder has the necessary files to work. I am not sure about this decission but I am leaving that in this way in this moment. Also I have changed some of the assets folder structure because of this general modifications in the project. I have changed the dependency of Normalize.css because the Stylus version that I was using it was not updated.

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,62 @@ var nib         = require('nib'),
     concat      = require('gulp-concat'),
     uglify      = require('gulp-uglify'),
     notify      = require("gulp-notify"),
+    modernizr   = require('gulp-modernizr'),
     critical    = require('critical').stream,
     runSequence = require('run-sequence').use(gulp);
+
+// The Init task it's for copy the javascript dependencies to the src folder.
+gulp.task('init', ['modernizr'], function() {
+    gulp.src('node_modules/picturefill/dist/picturefill.min.js')
+        .pipe(gulp.dest('src/assets/js'))
+    gulp.src('node_modules/jquery/dist/jquery.js')
+        .pipe(gulp.dest('src/assets/js/compile/vendor'))
+    gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(gulp.dest('src/assets/css/compile/vendor'))
+});
+
+gulp.task('modernizr', function() {
+    return gulp.src('src/assets/js/compile/*.js')
+        .pipe(modernizr(require('./modernizr.json')))
+        .pipe(gulp.dest('src/assets/js/compile/vendor'))
+});
 
 // Compile Stylus CSS
 gulp.task('style', function () {
     gulp.src('src/assets/css/styl/main.styl')
-        .pipe(stylus({compress: true, use: nib()}))
+        .pipe(stylus({
+            compress: true, 
+            use: nib(),
+            'include css': true
+        }))
         .pipe(rename('style.css'))
         .pipe(gulp.dest('src/assets/css'))
         .pipe(notify('CSS Compiled!'))
     ;
 });
 
-// Watch
-gulp.task('watch', function() {
-    gulp.watch('src/assets/js/main.js', ['js']);
-    gulp.watch('src/assets/css/styl/*.styl', ['style']);
+gulp.task('styles', function () {
+    gulp.src('wp-content/themes/vithas-indentia/assets/css/styl/main.styl')
+        .pipe(stylus({ compress: false, use: nib(), 'include css': true}))
+        .on('error', swallowError)
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('wp-content/themes/vithas-indentia/'))        
+    ;
 });
 
 // Generate Javascript
 gulp.task('js', function(){
-    return gulp.src(['bower_components/jquery/dist/jquery.min.js','bower_components/modernizr/modernizr.custom.js','src/assets/js/libraries/*.js'])
+    return gulp.src(['src/assets/js/compile/vendor/jquery.js','src/assets/js/compile/vendor/*.js','src/assets/js/compile/*.js'])
         .pipe(concat('javascript.js'))
         .pipe(gulp.dest('src/assets/js'))        
         .pipe(uglify())
         .pipe(gulp.dest('src/assets/js'));
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('src/assets/js/compile/*.js', ['js']);
+    gulp.watch('src/assets/css/styl/*.styl', ['style']);
 });
 
 // Generate & Inline Critical-path CSS
@@ -59,16 +89,14 @@ gulp.task('cleanBuild', function () {
 gulp.task('copy', function() {
     gulp.src('src/assets/css/style.css')
         .pipe(gulp.dest('build/assets/css/'))
-    gulp.src('src/assets/js/javascript.js')
-        .pipe(gulp.dest('build/assets/js/'))
-    gulp.src('bower_components/picturefill/dist/picturefill.min.js')
-        .pipe(gulp.dest('build/assets/js/'))
+    gulp.src('src/assets/js/*.js')
+        .pipe(gulp.dest('build/assets/js/'))    
     gulp.src('src/assets/img/*')
         .pipe(gulp.dest('build/assets/img/'))
 });
 
 gulp.task('build', function(callback) {
-    runSequence('style', 'js', 'critical', 'copy', callback);    
+    runSequence('style', 'js', 'critical', 'copy', callback);
 });
 
 // Default gulp task to run 
