9068c40cfcbf6452c3e4ec1f1eaf3f802e7f4f52
Miguel Morera <info@miguelmorera.com>
2020-10-27 22:22:40 +0100

    Changed "src" folder for "source" to be more accurate respect the other naming in the project.

--- gulpfile.js
+++ gulpfile.js
@@ -12,33 +12,33 @@ var nib         = require('nib'),
 
 // Compile Stylus CSS
 gulp.task('style', function (done) {
-    gulp.src('src/assets/css/compile/styl/style.styl')
+    gulp.src('source/assets/css/compile/styl/style.styl')
         .pipe(stylus({
             use: nib(),
             'include css': true
         }))
         .pipe(cleanCSS())
         .pipe(rename('style.css'))
-        .pipe(gulp.dest('src/assets/css'))
+        .pipe(gulp.dest('source/assets/css'))
         .pipe(notify('CSS Compiled!'));
     done();
 });
 
 // Generate Javascript
 gulp.task('js', function(done){
-    return gulp.src(['src/assets/javascript/compile/*.js'])
+    return gulp.src(['source/assets/javascript/compile/*.js'])
         .pipe(concat('javascript.js'))
-        .pipe(gulp.dest('src/assets/javascript'))
+        .pipe(gulp.dest('source/assets/javascript'))
         .pipe(notify('JS Compiled!'))
         .pipe(uglify())
-        .pipe(gulp.dest('src/assets/javascript'));
+        .pipe(gulp.dest('source/assets/javascript'));
     done();
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('src/assets/javascript/compile/*.js', gulp.series('js'));
-    gulp.watch('src/assets/css/compile/styl/*.styl', gulp.series('style'));
+    gulp.watch('source/assets/javascript/compile/*.js', gulp.series('js'));
+    gulp.watch('source/assets/css/compile/styl/*.styl', gulp.series('style'));
 });
 
 gulp.task('cleanBuild', function () {
@@ -46,13 +46,13 @@ gulp.task('cleanBuild', function () {
 });
 
 gulp.task('copy', function(done) {
-    gulp.src('src/*.html')
+    gulp.src('source/*.html')
         .pipe(gulp.dest('build/'))
-    gulp.src('src/assets/css/style.css')
+    gulp.src('source/assets/css/style.css')
         .pipe(gulp.dest('build/assets/css/'))
-    gulp.src('src/assets/javascript/*.js')
+    gulp.src('source/assets/javascript/*.js')
         .pipe(gulp.dest('build/assets/javascript/'))
-    gulp.src('src/assets/images/*')
+    gulp.src('source/assets/images/*')
         .pipe(gulp.dest('build/assets/images/'))
         .pipe(notify('All the resources has been copied to the build folder.'));
     done();
