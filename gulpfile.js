056f58bd30d7300e5fd1c19450de6fb91b86ab62
Miguel Morera <info@miguelmorera.com>
2020-10-27 23:56:31 +0100

    Favicon set and task added to the repo. The path for the javascript file has been corrected.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var nib         = require('nib'),
     uglify      = require('gulp-uglify'),
     notify      = require("gulp-notify"),
     cleanCSS    = require('gulp-clean-css'),
+    realFavicon = require('gulp-real-favicon'),
     runSequence = require('gulp4-run-sequence');
 
 // Compile Stylus CSS
@@ -41,6 +42,71 @@ gulp.task('watch', function() {
     gulp.watch('source/assets/css/compile/styl/*.styl', gulp.series('style'));
 });
 
+gulp.task('generate-favicon', function(done) {
+    // File where the favicon markups are stored (unnecessary but I don't know how to avoid its generation).
+    var FAVICON_DATA_FILE = 'source/assets/images/favicons/faviconData.json';
+    realFavicon.generateFavicon({
+        masterPicture: 'source/assets/images/favicons/favicon-master.png',
+        dest: 'source/assets/images/favicons',
+        iconsPath: 'source/assets/images/favicons/',
+        design: {
+            ios: {
+                pictureAspect: 'noChange',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#2b5797',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: false,
+                        rectangle: false
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'blackAndWhite',
+                threshold: 50,
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function() {
+        done();
+    });
+});
+
 gulp.task('cleanBuild', function () {
     return del('build');
 });
@@ -52,7 +118,7 @@ gulp.task('copy', function(done) {
         .pipe(gulp.dest('build/assets/css/'))
     gulp.src('source/assets/javascript/*.js')
         .pipe(gulp.dest('build/assets/javascript/'))
-    gulp.src('source/assets/images/*')
+    gulp.src('source/assets/images/*/*')
         .pipe(gulp.dest('build/assets/images/'))
         .pipe(notify('All the resources has been copied to the build folder.'));
     done();
