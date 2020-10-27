9a20c6c2d2ef4d2b5db17ef0cd879772bf10d0b0
Miguel Morera <info@miguelmorera.com>
2020-10-27 22:06:04 +0100

    Gulpfile and package.json updated according to the last changes made in the repo.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var nib         = require('nib'),
     del         = require('del'),
     gulp        = require('gulp'),
-    log         = require('fancy-log'),
     watch       = require('gulp-watch'),
     stylus      = require('gulp-stylus'),
     rename      = require("gulp-rename"),
@@ -11,20 +10,9 @@ var nib         = require('nib'),
     cleanCSS    = require('gulp-clean-css'),
     runSequence = require('gulp4-run-sequence');
 
-// The Init task it's for copy the javascript dependencies to the src folder.
-gulp.task('init', function() {
-    gulp.src('node_modules/picturefill/dist/picturefill.min.js')
-        .pipe(gulp.dest('src/assets/js'))
-    gulp.src('node_modules/jquery/dist/jquery.js')
-        .pipe(gulp.dest('src/assets/js/compile/vendor'))
-    gulp.src('node_modules/normalize.css/normalize.css')
-        .pipe(gulp.dest('src/assets/css/compile/vendor'))
-        .pipe(notify('The initialization of the project has been succesful! (PictureFill/jQuery/Normalize) copied to its folders.'))
-});
-
 // Compile Stylus CSS
 gulp.task('style', function (done) {
-    gulp.src('src/assets/css/compile/styl/main.styl')
+    gulp.src('src/assets/css/compile/styl/style.styl')
         .pipe(stylus({
             use: nib(),
             'include css': true
@@ -38,18 +26,18 @@ gulp.task('style', function (done) {
 
 // Generate Javascript
 gulp.task('js', function(done){
-    return gulp.src(['src/assets/js/compile/vendor/jquery.js','src/assets/js/compile/vendor/*.js','src/assets/js/compile/*.js'])
+    return gulp.src(['src/assets/javascript/compile/*.js'])
         .pipe(concat('javascript.js'))
-        .pipe(gulp.dest('src/assets/js'))
+        .pipe(gulp.dest('src/assets/javascript'))
         .pipe(notify('JS Compiled!'))
         .pipe(uglify())
-        .pipe(gulp.dest('src/assets/js'));
+        .pipe(gulp.dest('src/assets/javascript'));
     done();
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch('src/assets/js/compile/*.js', gulp.series('js'));
+    gulp.watch('src/assets/javascript/compile/*.js', gulp.series('js'));
     gulp.watch('src/assets/css/compile/styl/*.styl', gulp.series('style'));
 });
 
@@ -62,10 +50,10 @@ gulp.task('copy', function(done) {
         .pipe(gulp.dest('build/'))
     gulp.src('src/assets/css/style.css')
         .pipe(gulp.dest('build/assets/css/'))
-    gulp.src('src/assets/js/*.js')
-        .pipe(gulp.dest('build/assets/js/'))
-    gulp.src('src/assets/img/*')
-        .pipe(gulp.dest('build/assets/img/'))
+    gulp.src('src/assets/javascript/*.js')
+        .pipe(gulp.dest('build/assets/javascript/'))
+    gulp.src('src/assets/images/*')
+        .pipe(gulp.dest('build/assets/images/'))
         .pipe(notify('All the resources has been copied to the build folder.'));
     done();
 });
