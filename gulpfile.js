b53cbdde6f8d818a2f62d868fa25b53cba9eaad0
Miguel Morera <info@miguelmorera.com>
2025-03-20 20:35:58 +0100

    Removed Gulp4Sequence package. Improved tasks handling. The build task now gets all the files in the source folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,64 @@
-var del         = require('del'),
-    gulp        = require('gulp'),
-    watch       = require('gulp-watch'),
-    stylus      = require('gulp-stylus'),
-    rename      = require("gulp-rename"),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    notify      = require("gulp-notify"),
-    runSequence = require('gulp4-run-sequence');
+const del = require('del'),
+    gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    stylus = require('gulp-stylus'),
+    rename = require("gulp-rename"),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    notify = require("gulp-notify");
 
 // Compile Stylus CSS
-gulp.task('style', function (done) {
-    gulp.src('source/assets/css/compile/styl/style.styl')
+gulp.task('style', function () {
+    return gulp.src('source/assets/css/compile/styl/style.styl')
         .pipe(stylus({
-            compress : true,
+            compress: true,
             'include css': true
         }))
         .pipe(rename('style.css'))
         .pipe(gulp.dest('source/assets/css'))
-        .pipe(notify('CSS Compiled!'));
-    done();
+        .pipe(notify({ message: 'CSS Compiled!', onLast: true }));
 });
 
 // Generate Javascript
-gulp.task('js', function(done){
+gulp.task('js', function () {
     return gulp.src(['source/assets/javascript/compile/*.js'])
         .pipe(concat('javascript.js'))
         .pipe(gulp.dest('source/assets/javascript'))
-        .pipe(notify('JS Compiled!'))
         .pipe(uglify())
-        .pipe(gulp.dest('source/assets/javascript'));
-    done();
+        .pipe(gulp.dest('source/assets/javascript'))
+        .pipe(notify({ message: 'JS Compiled!', onLast: true }));
 });
 
 // Watch
-gulp.task('watch', function() {
-    gulp.watch('source/assets/javascript/compile/*.js', gulp.series('js'));
-    gulp.watch('source/assets/css/compile/styl/*.styl', gulp.series('style'));
+gulp.task('watch', function () {
+    gulp.watch('source/assets/javascript/compile/**/*.js', gulp.series('js'));
+    gulp.watch('source/assets/css/compile/styl/**/*.styl', gulp.series('style'));
 });
 
+// Delete Build Folder
 gulp.task('cleanBuild', function () {
     return del('build');
 });
 
-gulp.task('copy', function(done) {
-    gulp.src('source/*.html')
-        .pipe(gulp.dest('build/'))
-    gulp.src('source/assets/css/style.css')
-        .pipe(gulp.dest('build/assets/css/'))
-    gulp.src('source/assets/javascript/*.js')
-        .pipe(gulp.dest('build/assets/javascript/'))
-    gulp.src('source/assets/images/*')
-        .pipe(gulp.dest('build/assets/images/'))
-    gulp.src('source/assets/images/*/*')
-        .pipe(gulp.dest('build/assets/images/'))
-        .pipe(notify('All the resources has been copied to the build folder.'));
-    done();
+// Copy files to Build Folder
+gulp.task('copy', function () {
+    return gulp.src([
+        'source/**/*.html',
+        'source/assets/css/style.css',
+        'source/assets/javascript/**/*.js',
+        '!source/assets/javascript/compile/**',
+        'source/assets/images/**/*'
+    ], { base: 'source' })
+        .pipe(gulp.dest('build'))
+        .pipe(notify({ message: 'All resources have been copied to the build folder.', onLast: true }));
 });
 
-gulp.task('build', function(callback) {
-    runSequence('cleanBuild','style', 'js', 'copy', callback);
-});
+// Build Task
+gulp.task('build', gulp.series(
+    'cleanBuild',
+    gulp.parallel('style', 'js'),
+    'copy'
+));
 
-// Default gulp task to run 
+// Default task
 gulp.task('default', gulp.series('watch'));
